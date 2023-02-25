b87f98f408fc9dbc9f43a9d7caa46f4912b7643e
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2023-02-25 01:27:59 -0500

    feat(argocd): add plugins

--- terraform/argocd/argocd.tf
+++ terraform/argocd/argocd.tf
@@ -1,3 +1,29 @@
+resource "kubernetes_config_map_v1" "argocd_helm_plugin" {
+  metadata {
+    name      = "helm-plugin-config"
+    namespace = "argocd"
+  }
+
+  data = {
+    "plugin.yaml"       = file("${path.module}/files/plugins/helm/plugin.yaml")
+    "generate.sh"       = file("${path.module}/files/plugins/helm/generate.sh")
+    "get-parameters.sh" = file("${path.module}/files/plugins/get-parameters.sh")
+  }
+}
+
+resource "kubernetes_config_map_v1" "argocd_helm_kustomize_plugin" {
+  metadata {
+    name      = "helm-kustomize-plugin-config"
+    namespace = "argocd"
+  }
+
+  data = {
+    "plugin.yaml"       = file("${path.module}/files/plugins/helm-kustomize/plugin.yaml")
+    "generate.sh"       = file("${path.module}/files/plugins/helm-kustomize/generate.sh")
+    "get-parameters.sh" = file("${path.module}/files/plugins/get-parameters.sh")
+  }
+}
+
 resource "helm_release" "argocd" {
   name        = "argocd"
   namespace   = "argocd"
@@ -9,13 +35,19 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   version    = var.argocd_helm_version
 
+  values = [
+    file("${path.module}/files/manifests/argocd/repo-server.yaml"),
+  ]
+
   set {
     name  = "crds.install"
     value = "false"
   }
 
   depends_on = [
-    kubernetes_manifest.argocd_crds
+    kubernetes_manifest.argocd_crds,
+    kubernetes_config_map_v1.argocd_helm_plugin,
+    kubernetes_config_map_v1.argocd_helm_kustomize_plugin,
   ]
 }
 
