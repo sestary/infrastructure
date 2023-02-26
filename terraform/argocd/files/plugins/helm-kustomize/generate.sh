552f59ee7ede0c49bc4133d895b4c538ce4aba79
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2023-02-26 16:07:59 -0500

    feat(cert-manager): add cert-manager with cloudflare origin

--- terraform/argocd/files/plugins/helm-kustomize/generate.sh
+++ terraform/argocd/files/plugins/helm-kustomize/generate.sh
@@ -3,4 +3,4 @@
 ARGUMENTS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values-files").array | .[] | "--values=" + .')
 PARAMETERS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "helm-parameters").map | to_entries | map("\(.key)=\(.value)") | .[] | "--set=" + .')
 
-echo ". $ARGUMENTS $PARAMETERS" | xargs helm template | 
+echo ". $ARGUMENTS $PARAMETERS" | xargs helm template > all.yaml && kustomize build
