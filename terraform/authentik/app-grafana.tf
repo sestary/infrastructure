36b5729f0f037aebdfe19cbbcfc827417500b835
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2023-03-15 17:29:11 -0500

    feat(grafana): add Grafana app

---
+++ terraform/authentik/app-grafana.tf
@@ -0,0 +1,26 @@
+resource "authentik_application" "grafana" {
+  name  = "Grafana"
+  slug  = "grafana"
+  group = "Systems"
+
+  meta_description = "Grafana is a multi-platform open source analytics and interactive visualization web application."
+  meta_icon        = "/static/dist/media/grafana.png"
+  meta_launch_url  = "https://grafana.sestary.eu"
+  meta_publisher   = "Grafana"
+}
+
+resource "authentik_group" "grafana_admins" {
+  name = "grafana-admins"
+
+  lifecycle {
+    ignore_changes = [
+      users,
+    ]
+  }
+}
+
+resource "authentik_policy_binding" "grafana_group_admins" {
+  target = authentik_application.grafana.uuid
+  group  = authentik_group.grafana_admins.id
+  order  = 0
+}
