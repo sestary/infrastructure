9d3072a3d1c443dbcbaa850b825c354e95111b4c
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2023-03-18 11:23:02 -0500

    feat(outpost): move outposts to main namespaces

--- terraform/authentik/app-truenas.tf
+++ terraform/authentik/app-truenas.tf
@@ -6,8 +6,8 @@ resource "authentik_application" "truenas_nas1" {
   meta_description = "TrueNAS SCALE is an Open Source Hyperconverged Infrastructure solution."
   meta_icon        = "/static/dist/media/truenas.png"
   meta_launch_url  = "https://nas.on.sestary.eu" # This is wrong right now waiting for this fix to go in https://github.com/goauthentik/authentik/pull/4957
- 
-  meta_publisher   = "TrueNAS"
+
+  meta_publisher = "TrueNAS"
 }
 
 resource "authentik_application" "truenas_nas2" {
--- terraform/authentik/outpost.tf
+++ terraform/authentik/outpost.tf
@@ -1,15 +1,64 @@
+locals {
+  outpost_config = {
+    log_level                     = "info"
+    docker_labels                 = null
+    authentik_host                = "https://sso.sestary.eu/"
+    docker_network                = null
+    container_image               = null
+    docker_map_ports              = true
+    kubernetes_replicas           = 2
+    kubernetes_namespace          = "auth-authentik"
+    authentik_host_browser        = ""
+    object_naming_template        = "ak-outpost-%(name)s"
+    authentik_host_insecure       = false
+    kubernetes_service_type       = "ClusterIP"
+    kubernetes_image_pull_secrets = []
+    kubernetes_ingress_class_name = null
+    kubernetes_disabled_components = []
+    kubernetes_ingress_annotations = {}
+    kubernetes_ingress_secret_name = "ingress-tls"
+  }
+}
+
 resource "authentik_service_connection_kubernetes" "sestary" {
   name  = "sestary"
   local = true
 }
 
-resource "authentik_outpost" "sestary" {
-  name = "sestary-outpost"
+resource "authentik_outpost" "prometheus" {
+  name = "sestary-prometheus"
+
+  config = jsonencode(
+    merge(
+      local.outpost_config,
+      {
+        kubernetes_namespace = "monitoring-prometheus"
+      }
+    )
+  )
+
+  protocol_providers = [
+    authentik_provider_proxy.prometheus.id,
+    authentik_provider_proxy.prometheus_alerts.id,
+  ]
+
+  service_connection = authentik_service_connection_kubernetes.sestary.id
+}
+
+resource "authentik_outpost" "longhorn" {
+  name = "sestary-longhorn"
+
+  config = jsonencode(
+    merge(
+      local.outpost_config,
+      {
+        kubernetes_namespace = "storage-longhorn"
+      }
+    )
+  )
 
   protocol_providers = [
     authentik_provider_proxy.longhorn.id,
-    authentik_provider_proxy.prometheus.id,
-    authentik_provider_proxy.prometheus_alerts.id,
   ]
 
   service_connection = authentik_service_connection_kubernetes.sestary.id
