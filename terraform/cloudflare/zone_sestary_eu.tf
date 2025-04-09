c869a1badbe049d9fa167ce5002d6fc567c4a634
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2025-04-09 18:53:26 -0400

    fix: upgrade cloudflare provider (#138)

--- terraform/cloudflare/zone_sestary_eu.tf
+++ terraform/cloudflare/zone_sestary_eu.tf
@@ -1,79 +1,94 @@
 resource "cloudflare_zone" "sestary_eu" {
-  account_id = data.cloudflare_accounts.sestary.accounts[0].id
-  zone       = "sestary.eu"
+  account = {
+    id = data.cloudflare_accounts.sestary.result[0].id
+  }
+
+  name = "sestary.eu"
 }
 
 # Note: A bunch of the records in this zone are managed Dynamically
 
 # CAA Records
-resource "cloudflare_record" "caa_email" {
+resource "cloudflare_dns_record" "caa_email" {
   zone_id = cloudflare_zone.sestary_eu.id
 
   name = "sestary.eu"
-  data {
-    flags = "0"
+  data = {
+    flags = 0
     tag   = "iodef"
     value = "mailto:systems@sestary.eu"
   }
   type = "CAA"
 
   proxied = false
+
+  ttl = 3600
 }
 
-resource "cloudflare_record" "caa_letsencrypt" {
+resource "cloudflare_dns_record" "caa_letsencrypt" {
   zone_id = cloudflare_zone.sestary_eu.id
 
   name = "sestary.eu"
-  data {
-    flags = "0"
+  data = {
+    flags = 0
     tag   = "issue"
     value = "letsencrypt.org"
   }
   type = "CAA"
 
   proxied = false
+
+  ttl = 3600
 }
 
 # Localhost Record
-resource "cloudflare_record" "localhost" {
+resource "cloudflare_dns_record" "localhost" {
   zone_id = cloudflare_zone.sestary_eu.id
 
-  name  = "localhost.sestary.eu"
-  value = "127.0.0.1"
-  type  = "A"
+  name    = "localhost.sestary.eu"
+  content = "127.0.0.1"
+  type    = "A"
 
   proxied = false
+
+  ttl = 3600
 }
 
 # Grafana
-resource "cloudflare_record" "grafana" {
+resource "cloudflare_dns_record" "grafana" {
   zone_id = cloudflare_zone.sestary_eu.id
 
-  name  = "grafana.sestary.eu"
-  value = "sestary.grafana.net"
-  type  = "CNAME"
+  name    = "grafana.sestary.eu"
+  content = "sestary.grafana.net"
+  type    = "CNAME"
 
   proxied = false
+
+  ttl = 3600
 }
 
 # Public Services
-resource "cloudflare_record" "requests" {
+resource "cloudflare_dns_record" "requests" {
   zone_id = cloudflare_zone.sestary_eu.id
 
-  name  = "requests.sestary.eu"
-  value = "gw.on.sestary.eu"
-  type  = "CNAME"
+  name    = "requests.sestary.eu"
+  content = "gw.on.sestary.eu"
+  type    = "CNAME"
 
   proxied = true
+
+  ttl = 3600
 }
 
-resource "cloudflare_record" "sso" {
+resource "cloudflare_dns_record" "sso" {
   zone_id = cloudflare_zone.sestary_eu.id
 
-  name  = "sso.sestary.eu"
-  value = "gw.on.sestary.eu"
-  type  = "CNAME"
+  name    = "sso.sestary.eu"
+  content = "gw.on.sestary.eu"
+  type    = "CNAME"
 
   proxied = true
+
+  ttl = 3600
 }
 
