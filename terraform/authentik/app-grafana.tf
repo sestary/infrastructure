cbb3c4500c83118adec9944684e77d56262e5ccf
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2025-04-15 13:03:11 -0400

    fix: add grafana authentik

--- terraform/authentik/app-grafana.tf
+++ terraform/authentik/app-grafana.tf
@@ -1,7 +1,34 @@
+resource "random_password" "client_secret_grafana" {
+  length  = 40
+  special = false
+}
+
+resource "authentik_provider_oauth2" "grafana" {
+  name          = "grafana"
+  client_id     = "grafana"
+  client_secret = random_password.client_secret_grafana.result
+
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict",
+      url           = "http://grafana.sestary.eu/login/generic_oauth",
+    },
+  ]
+
+  signing_key = authentik_certificate_key_pair.sso.id
+
+  authorization_flow = authentik_flow.authorization_implicit_consent.uuid
+  invalidation_flow  = authentik_flow.invalidation.uuid
+
+  property_mappings = data.authentik_property_mapping_provider_scope.oidc.ids
+}
+
 resource "authentik_application" "grafana" {
   name  = "Grafana"
   slug  = "grafana"
   group = "Monitoring"
+
+  protocol_provider = authentik_provider_oauth2.grafana.id
 
   meta_description = "Grafana is a multi-platform open source analytics and interactive visualization web application."
   meta_icon        = "/static/dist/media/grafana.png"
@@ -17,6 +44,10 @@ resource "authentik_group" "grafana_admins" {
       users,
     ]
   }
+}
+
+data "authentik_provider_oauth2_config" "grafana" {
+  provider_id = authentik_provider_oauth2.grafana.id
 }
 
 resource "authentik_policy_binding" "grafana_group_admins" {
--- terraform/authentik/outputs.tf
+++ terraform/authentik/outputs.tf
@@ -9,6 +9,17 @@ output "argocd_oidc" {
   }
 }
 
+output "grafana_oidc" {
+  description = "Grafana OIDC config"
+  sensitive   = true
+
+  value = {
+    issuer        = data.authentik_provider_oauth2_config.grafana.issuer_url
+    client_id     = authentik_provider_oauth2.grafana.client_id
+    client_secret = authentik_provider_oauth2.grafana.client_secret
+  }
+}
+
 output "proxmox_oidc" {
   description = "Proxmox OIDC config"
   sensitive   = true
