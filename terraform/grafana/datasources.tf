ed3663adf4b1358ec9c990e20398c2933d366e90
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2025-05-03 11:18:06 -0400

    fix: grafana data source (#175)

--- terraform/grafana/datasources.tf
+++ terraform/grafana/datasources.tf
@@ -1,11 +1,14 @@
 resource "grafana_data_source" "prometheus" {
-  type                = "prometheus"
-  name                = "Sestary - Prometheus"
-  url                 = "http://prometheus-kube-prometheus-prometheus.monitoring-prometheus.svc.cluster.local:9090"
+  type = "prometheus"
+  name = "Sestary - Prometheus"
+  url  = "http://prometheus-kube-prometheus-prometheus.monitoring-prometheus.svc.cluster.local:9090"
 
   json_data_encoded = jsonencode({
-    httpMethod        = "POST"
-    prometheusType    = "Prometheus"
-    prometheusVersion = "3.3.0"
+    cacheLevel        = "High",
+    httpMethod        = "POST",
+    prometheusType    = "Prometheus",
+    prometheusVersion = "2.50.1"
   })
- }
+
+  private_data_source_connect_network_id = grafana_cloud_private_data_source_connect_network.on.id
+}
